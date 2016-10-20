b3ba321ccd2fe60ed65ba383dfb865d11c5a9558
Thomas Aglassinger <roskakori@users.sourceforge.net>
2016-10-20 09:01:10 +0200

    Added workaround for "python3 setup.py install" sometimes ignoring "install_requires". For a discussion why this happens see e.g. http://stackoverflow.com/questions/21915469/.

--- tests/count_pygount.sh
+++ tests/count_pygount.sh
@@ -6,6 +6,9 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 source tests/.temp/env_count/bin/activate
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
+pip3 install -r dev-requirements.txt
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 python3 setup.py install
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
