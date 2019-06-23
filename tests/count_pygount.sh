e19af5a59a517aa828234a5626116219f4f0d1bf
Thomas Aglassinger <roskakori@users.sourceforge.net>
2019-06-23 17:48:51 +0200

    #23 Cleaned up error handling.

--- tests/count_pygount.sh
+++ tests/count_pygount.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 # Install pygount in a new environment and count its lines of code.
+set -ex
 python3 -m venv tests/.temp/env_count
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
 source tests/.temp/env_count/bin/activate
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
-pip3 install -r dev-requirements.txt
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+pip3 install --upgrade pip
+pip3 install --requirement dev-requirements.txt
 
 python3 setup.py install
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-
 tests/.temp/env_count/bin/pygount --format=cloc-xml --out cloc.xml --verbose build.xml pygount setup.py tests
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+deactivate
