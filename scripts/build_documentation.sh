35a18dc7c00e1c8bf8e65998c387b1a11e8dcfae
Thomas Aglassinger <roskakori@users.sourceforge.net>
2023-06-27 11:17:29 +0200

    #116 Change build script to use separate virtual environment
    This enables the local build, the CI build and the ReadTheDocs build to have the same potential dependency conflicts and consequently detect the early on.

--- scripts/build_documentation.sh
+++ scripts/build_documentation.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
+echo "🌱 Setting up environment to build documentation"
+virtualenv .venv-sphinx
+. .venv-sphinx/bin/activate
+pip install -r docs/requirements.txt
 echo "📖 Building documentation"
 make -C docs html
 echo "🎉 Successfully built documentation in docs/_build/html/index.html"
