ba28621e22210a4f19e6d217c8a8ca48e6e9a5a1
Thomas Aglassinger <roskakori@users.sourceforge.net>
2024-05-13 19:54:21 +0200

    #122 Clean up title

--- scripts/build_movie.sh
+++ scripts/build_movie.sh
@@ -8,4 +8,4 @@
 # See also: <https://www.ekreative.com/blog/producing-your-own-git-repository-animated-visualization-video/>
 set -ex
 mkdir -p build
-gource --auto-skip-seconds 1 --file-idle-time 0 --hide dirnames,filenames,mouse --seconds-per-day 1 --title Siisurit -1920x1080 --output-ppm-stream - . | ffmpeg -y -r 30 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 /tmp/pygount_movie.mp4
+gource --auto-skip-seconds 1 --file-idle-time 0 --hide dirnames,filenames,mouse --seconds-per-day 1 --title Pygount -1920x1080 --output-ppm-stream - . | ffmpeg -y -r 30 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 /tmp/pygount_movie.mp4
