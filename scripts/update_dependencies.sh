63f60caf4b0cc2516551bc73cbda9a1e1621eb6e
Thomas Aglassinger <roskakori@users.sourceforge.net>
2026-05-12 07:32:55 +0200

    #221 Migrate ReadTheDocs to uv

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -6,8 +6,4 @@ uv sync
 uv lock --upgrade
 echo "🛠️ Updating pre-commit"
 uv run pre-commit autoupdate
-echo "📖 Updating documentation"
-# HACK This is only needed because ReadTheDocs cannot extract the
-#  dependencies from pyproject.toml.
-uv export --no-hashes --format requirements-txt > docs/requirements.txt
 echo "🎉 Successfully updated dependencies"
